64ee815533ee69acd71f64e9d3fc868666ca92ed
AV-Shell <avshell.sj@gmail.com>
2021-12-05 11:57:40 +0300

    init: start the task

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
 
+// Start server listen:
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
