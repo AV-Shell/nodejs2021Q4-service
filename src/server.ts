2b2386c3ab5ce3dd44f5fa6cb3d98d873f34d43b
AV-Shell <avshell.sj@gmail.com>
2021-12-26 21:35:55 +0300

    feat: add log several levels to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
 import { config } from './common/config';
 import app from './app';
+import { logger } from './common/logHandler';
+
+logger.log('error', 'error level log test');
+logger.log('warn', 'warn level log test');
+logger.log('info', 'info level log test');
+logger.log('verbose', 'verbose level log test');
+logger.log('debug', 'debug level log test');
+logger.log('silly', 'silly level log test');
+
+// throw Error('Oops!');
+
+// Promise.reject(Error('Oops!'));
 
 app.listen(config.PORT, () =>
   console.log(`App is running on http://localhost:${config.PORT}`)
