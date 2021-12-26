f4dd8cd10e5a7ad22f855fd329fefea984852251
AV-Shell <avshell.sj@gmail.com>
2021-12-26 21:32:43 +0300

    feat: add log level to env and to config

--- src/common/config.ts
+++ src/common/config.ts
@@ -12,7 +12,35 @@ interface IConfig {
   MONGO_CONNECTION_STRING: string;
   JWT_SECRET_KEY: string;
   AUTH_MODE: boolean;
+  FILE_LOG_LEVEL: number;
+  CONSOLE_LOG_LEVEL: number;
 }
+
+// interface ILogLevels {
+//   [key: number]: 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly';
+// }
+
+// export const logLevels: ILogLevels = {
+//   0: 'error',
+//   1: 'warn',
+//   2: 'info',
+//   3: 'verbose',
+//   4: 'debug',
+//   5: 'silly',
+// };
+
+export enum ELogLevel {
+  off = -1,
+  error = 0,
+  warn,
+  info,
+  verbose,
+  debug,
+  silly,
+}
+
+const fileLogLevel = +(process?.env?.FILE_LOG_LEVEL ?? 0);
+const consoleLogLevel = +(process.env?.CONSOLE_LOG_LEVEL ?? 0);
 
 export const config: IConfig = {
   PORT: +(process.env.PORT ?? 4000),
@@ -21,4 +49,12 @@ export const config: IConfig = {
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING ?? '',
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ?? '',
   AUTH_MODE: process.env.AUTH_MODE === 'true' ?? '',
+  FILE_LOG_LEVEL:
+    fileLogLevel >= ELogLevel.off && fileLogLevel <= ELogLevel.silly
+      ? fileLogLevel
+      : ELogLevel.off,
+  CONSOLE_LOG_LEVEL:
+    consoleLogLevel >= ELogLevel.off && consoleLogLevel <= ELogLevel.silly
+      ? consoleLogLevel
+      : ELogLevel.off,
 };
