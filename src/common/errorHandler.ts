bad2bfa898d9b8a3e22d6264fbca908d801ac95f
AV-Shell <avshell.sj@gmail.com>
2021-12-26 21:34:40 +0300

    feat: add logger to error handler

--- src/common/errorHandler.ts
+++ src/common/errorHandler.ts
@@ -1,35 +1,39 @@
 import { NextFunction, Request, Response } from 'express';
 import { MyCustomError } from './myCustomError';
 import { responseCode } from './statusCodes';
+import {
+  errorHandlerRequestLogger,
+  uncaughtExceptionLogger,
+  unhandledRejectionLogger,
+} from './logHandler';
 
 const { NOT_FOUND, INTERNAL_SERVER_ERROR } = responseCode;
 
 /**
- * Logs the error and terminates the process
- * @param err - error to be logged
- * @returns returns nothing, terminates the program.
- */
-function exceptionHandler(err: Error): void {
-  console.error(err.message);
-  process.exit(9);
-}
-
-/**
  * Callback for uncaught exceptions (errors)
  * @param err - error to be logged
+ * @param origin - exception origin to be logged
  * @returns returns nothing
  */
-export const uncaughtExceptionHandler = (err: Error): void => {
-  exceptionHandler(err);
+export const uncaughtExceptionHandler = (err: Error, origin: string): void => {
+  uncaughtExceptionLogger('UncaughtException', err, origin);
 };
 
 /**
  * Callback for unhandled rejections (promise reject)
  * @param err - error to be logged
+ * @param promise - rejected promise to be logged
  * @returns returns nothing
  */
-export const unhandledRejectionHandler = (err: Error): void => {
-  exceptionHandler(err);
+export const unhandledRejectionHandler = (
+  reason: Error,
+  promise: Promise<void>
+): void => {
+  unhandledRejectionLogger(
+    `Unhandled Rejection at: ${promise} ${JSON.stringify(promise, null, 2)} 
+    Error stack: ${reason.stack}
+    Error message: ${reason.message}`
+  );
 };
 
 /**
@@ -46,15 +50,12 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Midleware error handler ');
-  console.log('Request ', req.originalUrl);
-  console.log('error message', err.message);
   if (err instanceof MyCustomError) {
+    errorHandlerRequestLogger(err, req, err.myErrStatus);
     res.status(err.myErrStatus ?? NOT_FOUND).send(err.message);
-    console.log('error statuscode ', err.myErrStatus ?? NOT_FOUND);
-  } else {
-    console.log('error stack \r\n', err.stack);
-    res.status(INTERNAL_SERVER_ERROR).json('Something wrong');
+    return;
   }
+  errorHandlerRequestLogger(err, req, INTERNAL_SERVER_ERROR);
+  res.status(INTERNAL_SERVER_ERROR).json('Something wrong');
   next();
 };
