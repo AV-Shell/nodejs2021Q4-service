1f3813ac44b5ff5fe91d3eb728fda891f6a57a76
AV-Shell <avshell.sj@gmail.com>
2021-12-26 21:35:19 +0300

    feat: add logger midlware to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import { myLoggerReq } from './common/logHandler';
 import {
   errorHandler,
   uncaughtExceptionHandler,
@@ -16,6 +17,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(myLoggerReq);
+
 app.use('/users', userRouter);
 
 app.use('/boards', boardRouter);
@@ -25,4 +28,3 @@ boardRouter.use('/:boardId/tasks', taskRouter);
 app.use(errorHandler);
 
 export default app;
-
